f2b14225749d5a7e05b6982cbf1ccb25c5f014aa
ababacarvan <ababacarvan@gmail.com>
2026-01-05 12:30:23 +0100

    Implementation de calcule et hello

---
+++ app.py
@@ -0,0 +1,13 @@
+def hello(nom):
+    return f"Bonjour {nom} 👋"
+
+def say_hello():
+    return f"Bonjour 👋"
+
+
+def calcule(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(say_hello("Ababacar"))
+
---
+++ test_app.py
@@ -0,0 +1,17 @@
+import unittest
+from app import hello, calcule, say_hello
+
+
+class TestApp(unittest.TestCase):
+
+    def test_hello(self):
+        self.assertEqual(say_hello("Ababacar"), "Bonjour Ababacar 👋")
+
+    def test_calcule(self):
+        self.assertEqual(calcule(2, 3), 5)
+        self.assertEqual(calcule(-1, 1), 0)
+        self.assertEqual(calcule(0, 0), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
